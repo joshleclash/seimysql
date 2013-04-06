9533090ed3aa23eb12e0c04fdd35a2e225af37c5
juan.verano <juan.verano@ica.gov.co>
2013-04-06 15:06:32 -0500

    a

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-@session_start();
-include_once('../components/msgMail.php');
-define("PATCH","..");
-define("DEVELOPER","Todos los derechos reservados <br/>Designed By : joshleclash@gmail.com ".Components::getDate('America/Bogota','Y'));
-?>
-
